9b913916e0e7654c8bedb3554fb0c76791c18dcc
Jonathan Wang <jonathan.wang1996@gmail.com>
2021-06-17 22:48:04 -0500

    removed src folder

--- server-local.js
+++ server-local.js
@@ -2,6 +2,6 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const app = require("./src/express/server");
+const app = require("./express/server");
 
 app.listen(`${process.env.PORT}`, () => console.log(`Local app listening on port ${process.env.PORT}!`));
